3c641523c23d768fda7d4a3be8c4b572935dad66
Игорь <vaskinin97@gmail.com>
2024-07-10 12:38:42 +0400

    added ReferralCode model

--- rest_api/users/models.py
+++ rest_api/users/models.py
@@ -43,3 +43,17 @@ class Arcticle(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # models.py
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class ReferralCode(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=20, unique=True)
+    expiration_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_active(self):
+        return self.expiration_date > timezone.now()
