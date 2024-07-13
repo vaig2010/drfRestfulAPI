aa24b60a0ca6f038d80e5e2e782f7f91931452f0
Игорь <vaskinin97@gmail.com>
2024-07-13 14:05:03 +0400

    edit: UserViewSet

--- rest_api/rest_api/urls.py
+++ rest_api/rest_api/urls.py
@@ -18,23 +18,12 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers, serializers, viewsets
-from users.views import ArcticleViewSet, UserRegisterView, UserLoginView, ReferralCodeViewSet
+from users.views import (ArcticleViewSet, UserRegisterView, UserLoginView,
+                         UserViewSet, ReferralCodeViewSet)
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView
 )
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
