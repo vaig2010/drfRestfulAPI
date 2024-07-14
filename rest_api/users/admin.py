d9579820a4d0337e4fee4b3b8fd13c53240897ce
Игорь <vaskinin97@gmail.com>
2024-07-14 17:10:29 +0400

    added: all models in admin.py

--- rest_api/users/admin.py
+++ rest_api/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Arcticle
+from .models import Arcticle, Referral, ReferralCode
 
 admin.site.register(Arcticle)
+admin.site.register(Referral)
+admin.site.register(ReferralCode)
